d342bf16492a38a4c43fa3a10e1cdbba8618f849
lxq <1204041386@qq.com>
2015-07-14 10:41:23 +0800

    数据库修改了

--- src/com/example/Database/SQLiteConstants.java
+++ src/com/example/Database/SQLiteConstants.java
@@ -24,19 +24,20 @@ public class SQLiteConstants {
 	/*
 	 * 建立病历查询表
 	 */
-	public final static String CREATE_TABLE_LOOKUPCASE="create table if not exists lookupcase(_id integer primary key autoincrement,account varchar(20),name varchar(20)" +
+	public final static String CREATE_TABLE_LOOKUPCASE="create table if not exists lookupcase(_id integer primary key autoincrement," +
+			"account varchar(20),name varchar(20)," +
 			"age integer,sex varchar(10),id varchar(20),date varchar(20),m_information varchar(2))";
 	/**
 	 * 建立病人费用明细表
 	 */
-	public final static String CREATE_TABLE_OWNEXPENSE="create table if no exists ownexpense(_id integer primary key autoincrement,date varchar(20)" +
+	public final static String CREATE_TABLE_OWNEXPENSE="create table if not exists ownexpense(_id integer primary key autoincrement,date varchar(20)" +
 			",account varchar(20),name varchar(20),age varchar(20),sex varchar(20), ward varchar(20),ward_no varchar(20),expense_item varchar(20)," +
 			"expense_code varchar(20),expense_name varchar(20),unit_price varchar(20),gauge_unit varchar(20),total_money varchar(20))";
 	public final static String account="account";
 	/**
 	 * 床位信息统计表
 	 */
-	public final static String CREATE_TABLE_WARDBED="create table if no exists ward_bed(_id integer primary key autoincrement,ward_no varchar(20),doctor varchar(20),nurse varchar(20)," +
+	public final static String CREATE_TABLE_WARDBED="create table if not exists ward_bed(_id integer primary key autoincrement,ward_no varchar(20),doctor varchar(20),nurse varchar(20)," +
 			"patient varchar(20),bed_price_list varchar(20))";
 	public final static String password="password";
 	public final static String table_login="login";
